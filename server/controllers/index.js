709e84535cf11d0b48193a11ca56b5b6ee50f6c7
masato <masato@prosperworks.com>
2017-08-08 18:59:56 -0700

    test

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -2,16 +2,21 @@ var models = require('../models');
 
 module.exports = {
   messages: {
-    get: function(req, res) {
+    get: function (req, res) {
       models.messages.get();
     }, // a function which handles a get request for all messages
-    post: function(req, res) {} // a function which handles posting a message to the database
+    post: function (req, res) {
+      console.log("Post Called");
+      // req.on('data', function (data) {
+      //   console.log(data);
+      // });
+    } // a function which handles posting a message to the database
   },
 
   users: {
     // Ditto as above
-    get: function(req, res) {},
-    post: function(req, res) {}
+    get: function (req, res) {},
+    post: function (req, res) {}
   }
 };
 //models.messages.get();
